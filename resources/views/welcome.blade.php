93d08b2892351a8cf946bb089f760a71c827a7b7
Daniel Sundermeier <daniel@hof-sundermeier.de>
2020-11-11 04:15:09 +0100

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,9 +3,13 @@
         Welcome
     </x-slot>
 
-    <p>Ich programmiere <a href="https://www.serienguide.tv" target="_blank">serienguide</a> komplett neu. Hier ist der aktuelle Stand.</p>
+    <p>Ich programmiere <a href="https://www.serienguide.tv" target="_blank">serienguide</a> komplett neu. Dies ist der aktuelle Stand.</p>
     <br />
     <p>Die Datenbank wird regelmäßig zurückgesetzt.</p>
+    <br />
+    <p>serienguide ist Open Source. <a class="underline" href="https://github.com/serienguide/serienguide" target="_blank">Github</a></p>
+    <br />
+    <p>Feedback, Fehler und Ideen bitte <a class="underline" href="https://github.com/serienguide/serienguide/issues" target="_blank">hier</a> eintragen.</p>
     <br />
     <p>Anmelden:</p>
     <p>user@example.com</p>
