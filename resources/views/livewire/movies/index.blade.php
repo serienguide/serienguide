15806bac854149ae2620a66da75c53bcbf22126a
Daniel Sundermeier <daniel@hof-sundermeier.de>
2020-11-28 07:50:58 +0100

    added: media card deck

--- resources/views/livewire/movies/index.blade.php
+++ resources/views/livewire/movies/index.blade.php
@@ -5,16 +5,5 @@
             <input wire:model.debounce.250ms="filter.search" id="search" class="form-input block w-full sm:text-sm sm:leading-5" placeholder="Suche">
         </div>
     </div>
-    <div wire:loading.delay class="text-center w-100 p-5">
-        Lade Daten.
-    </div>
-    <ul class="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 mb-3">
-        @forelse ($items as $item)
-            @livewire('media.card', ['model' => $item], key('media-card-' . $item->id))
-        @empty
-            Keine Filme vorhanden
-        @endforelse
-    </ul>
-
-    {{ $items->links() }}
+    <x-media.cards.deck :items="$items"/>
 </div>
