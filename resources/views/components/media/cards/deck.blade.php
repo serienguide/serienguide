3fee6a552f90bbc7d039f5590582e0f092e70837
Daniel Sundermeier <daniel@hof-sundermeier.de>
2020-11-28 07:55:15 +0100

    Update deck.blade.php

--- resources/views/components/media/cards/deck.blade.php
+++ resources/views/components/media/cards/deck.blade.php
@@ -1,5 +1,5 @@
 <div wire:loading.delay class="text-center w-100 p-5">
-    Lade Daten.
+    <i class="fa fa-spinner fa-spin fa-3x text-grey"></i>
 </div>
 <ul class="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 mb-3">
     @forelse ($items as $item)
