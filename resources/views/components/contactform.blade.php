6e807a6e924302236d7685f9642a6c1c963a3761
Daniel Sundermeier <daniel@hof-sundermeier.de>
2020-12-05 12:35:33 +0100

    updated: contactform

--- resources/views/components/contactform.blade.php
+++ resources/views/components/contactform.blade.php
@@ -16,7 +16,7 @@
         <x-forms.input name="mail" label="E-Mail" type="email" />
     </div>
     <div class="py-3">
-        <x-forms.textarea name="message" label="Nachricht" />
+        <x-forms.textarea rows="5" name="message" label="Nachricht" />
     </div>
     <div class="py-3">
         <button type="submit" class="w-full inline-flex items-center justify-center px-6 py-3 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
--- resources/views/components/forms/textarea.blade.php
+++ resources/views/components/forms/textarea.blade.php
@@ -5,9 +5,7 @@
         <label for="{{ $attributes['name'] }}" class="block text-sm font-medium text-gray-700">{{ $label }}</label>
     @endif
     <div class="mt-1">
-        <textarea {!! $attributes->merge(['class' => 'py-3 px-4 block w-full shadow-sm focus:ring-blue-500 focus:border-blue-500 border-gray-300 rounded-md']) !!} id="{{ $attributes['name'] }}" aria-describedby="{{ $attributes['name'] }}-description">
-            {{ $value }}
-        </textarea>
+        <textarea {!! $attributes->merge(['class' => 'py-3 px-4 block w-full shadow-sm focus:ring-blue-500 focus:border-blue-500 border-gray-300 rounded-md']) !!} id="{{ $attributes['name'] }}" aria-describedby="{{ $attributes['name'] }}-description">{{ $value }}</textarea>
         @error($attributes['name'])
             <p class="mt-2 text-sm text-red-600" id="{{ $attributes['name'] }}-error">{{ $message }}</p>
         @enderror
