45cf3eb88b9f37ea25df0b0c8c66252b45e60afd
Daniel Sundermeier <daniel@hof-sundermeier.de>
2021-01-07 09:21:22 +0100

    updated: user show view

--- resources/views/users/profiles/show.blade.php
+++ resources/views/users/profiles/show.blade.php
@@ -1,63 +1,60 @@
 <x-app-layout>
-
-    <section class="relative w-screen bg-no-repeat bg-cover bg-top" style="height: 50vh; min-height: 500px;  @isset ($user->last_watched) background-image: url({{ $user->last_watched->watchable->backdrop_url_xl }}) @endisset" >
+    <section class="relative w-screen bg-no-repeat bg-cover bg-top" style="height: 30vh; min-height: 250px;  @isset ($user->last_watched) background-image: url({{ $user->last_watched->watchable->backdrop_url_xl }}) @endisset" >
         <div class="absolute w-screen bottom-0 h-72" style="background-image: linear-gradient(to bottom, rgba(0,0,0,0) 0%, rgba(0,0,0,0.6) 100%);"></div>
-            <div class="absolute w-screen bottom-0 text-white font-bold">
-                <x-container class="py-4 pt-16">
-                    <div class="flex items-end">
-                        <img class="h-48 w-48 rounded-full" src="https://www.gravatar.com/avatar/{{ md5($user->email) }}">
-                        <div class="flex flex-col ml-3">
-                            <div class="flex items-center">
-                                <h1 class="flex-grow text-2xl font-bold">{{ $user->name }}</h1>
-                                <div>
-                                    @env('local')
-                                        <span class="inline-flex items-center px-3 py-0.5 rounded-full text-sm font-bold bg-green-100 text-green-800">
-                                            Level
-                                        </span>
-                                    @endenv
-                                </div>
-                            </div>
-                            @env('local')
-                                <div class="py-4 text-center">
-                                    <button type="button" class="inline-flex items-center px-4 py-2 text-sm leading-5 font-medium rounded-md bg-blue-700 text-white hover:bg-blue-500 active:bg-blue-800 active:bg-gray-50 transition ease-in-out duration-150">
-                                        Folgen
-                                    </button>
-                                </div>
-                                <div class="flex items-center text-center">
-                                    <div class="mr-3">
-                                        <div class="text-2xl">XX</div>
-                                        <div class="text-xs text-gray-500">FOLLOWER</div>
-                                    </div>
-                                    <div>
-                                        <div class="text-2xl">XX</div>
-                                        <div class="text-xs text-gray-500">FOLLOWING</div>
-                                    </div>
-                                </div>
-                            @endenv
-                        </div>
-                        <div class="flex-grow text-right pr-3 hidden md:block">
-                            @isset($user->last_watched)
-                                <a href="{{ $user->last_watched->watchable->path }}" class="text-sm text-white font-bold">
-                                    @if ($user->last_watched->watchable->is_episode)
-                                        {{ $user->last_watched->watchable->show->name }} {{ $user->last_watched->watchable->season->season_number }}x{{ $user->last_watched->watchable->episode_number }}
-                                    @else
-                                        {{ $user->last_watched->watchable->name }}
-                                    @endif
-                                </a>
-                            @endisset
-                        </div>
+        <div class="absolute w-screen bottom-0 text-white font-bold">
+            <x-container class="py-4 pt-16">
+                <div class="flex items-end">
+
+                    <div class="flex-grow text-right pr-3 hidden md:block">
+                        @isset($user->last_watched)
+                            <a href="{{ $user->last_watched->watchable->path }}" class="text-sm text-white font-bold">
+                                @if ($user->last_watched->watchable->is_episode)
+                                    {{ $user->last_watched->watchable->show->name }} {{ $user->last_watched->watchable->season->season_number }}x{{ $user->last_watched->watchable->episode_number }}
+                                @else
+                                    {{ $user->last_watched->watchable->name }}
+                                @endif
+                            </a>
+                        @endisset
                     </div>
-                </x-container>
-            </div>
+                </div>
+            </x-container>
+        </div>
+
     </section>
 
-    <section class="bg-gray-800 overflow-auto">
+    <section>
+        <x-container>
+            <div class="-mt-12 z-20 relative sm:-mt-16 sm:flex sm:items-end sm:space-x-5">
+                <div class="flex">
+                    <img class="h-24 w-24 rounded-full ring-4 ring-white sm:h-32 sm:w-32" src="https://www.gravatar.com/avatar/{{ md5($user->email) }}">
+                </div>
+                <div class="mt-6 sm:flex-1 sm:min-w-0 sm:flex sm:items-center sm:justify-end sm:space-x-6 sm:pb-1">
+                    <div class="mt-6 min-w-0 flex-1">
+                        <h1 class="text-2xl font-bold text-gray-900 truncate">
+                            {{ $user->name }}
+                        </h1>
+                    </div>
+                    <div class="mt-6 flex flex-col justify-stretch space-y-3 sm:flex-row sm:space-y-0 sm:space-x-4">
+                        @if (false)
+                            <button type="button" class="inline-flex justify-center px-4 py-2 border border-gray-300 shadow-sm text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-pink-500">
+                                <span>Folgen</span>
+                            </button>
+                        @endif
+                    </div>
+                </div>
+            </div>
+        </x-container>
+    </section>
+
+    <section class="bg-gray-800 overflow-auto mt-6 sm:mt-2 2xl:mt-5">
         <x-container class="py-2">
             <div class="flex flex-nowrap justify-between intems-center">
                 <a class="px-3 py-2 rounded-md text-sm font-medium text-gray-300 hover:text-white hover:bg-gray-700" href="{{ $user->profile_path }}">Profil</a>
                 <a class="px-3 py-2 rounded-md text-sm font-medium text-gray-300 hover:text-white hover:bg-gray-700" href="{{ $user->profile_path }}/watched">Gesehen</a>
+                @env('local')
+                    <a class="px-3 py-2 rounded-md text-sm font-medium text-gray-300 hover:text-white hover:bg-gray-700" href="{{ $user->profile_path }}/progress">Fortschritt</a>
+                @endenv
                 <a class="px-3 py-2 rounded-md text-sm font-medium text-gray-300 hover:text-white hover:bg-gray-700" href="{{ $user->profile_path }}/rated">Bewertet</a>
-                <a class="px-3 py-2 rounded-md text-sm font-medium text-gray-300 hover:text-white hover:bg-gray-700" href=""></a>
                 <a class="px-3 py-2 rounded-md text-sm font-medium text-gray-300 hover:text-white hover:bg-gray-700" href=""></a>
                 <a class="px-3 py-2 rounded-md text-sm font-medium text-gray-300 hover:text-white hover:bg-gray-700" href=""></a>
                 <a class="px-3 py-2 rounded-md text-sm font-medium text-gray-300 hover:text-white hover:bg-gray-700" href=""></a>
