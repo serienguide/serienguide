33b4f65507fcb355a4964f3d7d2d500f7959df8a
Daniel Sundermeier <daniel@hof-sundermeier.de>
2021-01-13 10:58:48 +0100

    Update followers.blade.php

--- resources/views/livewire/users/profiles/followers.blade.php
+++ resources/views/livewire/users/profiles/followers.blade.php
@@ -21,13 +21,13 @@
                     <img class="h-10 w-10 rounded-full" src="https://www.gravatar.com/avatar/{{ md5($item->email) }}" alt="">
                 </div>
                 <div class="flex-1 min-w-0">
-                <a href="#" class="focus:outline-none">
+                <a href="{{ $item->profile_path }}" class="focus:outline-none">
                     <span class="absolute inset-0" aria-hidden="true"></span>
                         <p class="text-sm font-medium text-gray-900">
                             {{ $item->name }}
                         </p>
                         <p class="text-sm text-gray-500 truncate">
-                            {{ $item->followers_count }}
+                            {{ $item->followers_count }} Abonnenten
                         </p>
                     </a>
                 </div>
